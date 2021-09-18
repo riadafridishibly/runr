173a1f49e23d7daab9cc2b8f7bec842b18a9d504
Riad Afridi Shibly <riadafridishibly@gmail.com>
2021-09-19 02:33:39 +0600

    somewhat working

--- main.go
+++ main.go
@@ -2,27 +2,40 @@ package main
 
 import (
 	"context"
-	"errors"
 	"io"
 	"log"
 	"os"
 	"os/exec"
-	"time"
+	"os/signal"
 
 	"github.com/fsnotify/fsnotify"
 	"github.com/k0kubun/pp"
 )
 
-var ()
+type Watcher struct {
+	directories      []string
+	exclue           []string
+	operationToWatch fsnotify.Op // fsnotify.Write | fsnotify.Remove
+	done             chan bool
 
-func f() {
+	*fsnotify.Watcher
+}
+
+func NewWatcher(dirs ...string) *Watcher {
+	return &Watcher{
+		directories: dirs,
+		done:        make(chan bool),
+	}
+}
+
+func (w *Watcher) Watch(changed chan<- bool) {
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer watcher.Close()
 
-	done := make(chan bool)
+	w.Watcher = watcher
+
 	go func() {
 		for {
 			select {
@@ -33,22 +46,26 @@ func f() {
 				log.Println("event:", event)
 				if event.Op&fsnotify.Write == fsnotify.Write {
 					log.Println("modified file:", event.Name)
+					changed <- true
 				}
 			case err, ok := <-watcher.Errors:
 				if !ok {
 					return
 				}
 				log.Println("error:", err)
+			case <-w.done:
+				w.Watcher.Close()
+				return
 			}
 		}
 	}()
 
-	err = watcher.Add("/tmp/foo")
-	if err != nil {
-		log.Fatal(err)
+	for _, dir := range w.directories {
+		err = watcher.Add(dir)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
-	<-done
-
 }
 
 type Runner struct {
@@ -63,6 +80,7 @@ type Runner struct {
 }
 
 func NewRunnerWithDefault(command string, args ...string) *Runner {
+
 	return &Runner{
 		command: command,
 		args:    args,
@@ -73,20 +91,22 @@ func NewRunnerWithDefault(command string, args ...string) *Runner {
 }
 
 func (r *Runner) Run() {
-	ctx, cancel := context.WithCancel(context.Background())
+	ctx, cancel := context.WithCancel(context.TODO())
 	r.CancelFunc = cancel
-
 	cmd := exec.CommandContext(ctx, r.command, r.args...)
 	cmd.Stderr = r.stderr
 	cmd.Stdin = r.stdin
 	cmd.Stdout = r.stdout
 
 	go func() {
-		err := cmd.Run()
+		err := cmd.Start()
 		if err != nil {
-			if !errors.Is(err, context.Canceled) {
-				pp.Println(err)
-			}
+			pp.Println(err)
+			// fmt.Println("run error:", err.Error(), cmd.String())
+			// fmt.Println(cmd.ProcessState.SysUsage().(*syscall.Rusage).Maxrss)
+			// if !errors.Is(err, context.Canceled) {
+			// 	pp.Println(err)
+			// }
 		}
 	}()
 }
@@ -96,34 +116,31 @@ func (r *Runner) ReRun() {
 	r.Run()
 }
 
-func run(args ...string) (*exec.Cmd, context.CancelFunc) {
-	ctx, cancel := context.WithCancel(context.Background())
-	cmd := exec.CommandContext(ctx, args[0], args[:1]...)
-
-	return cmd, cancel
+func (r *Runner) Exit() {
+	r.CancelFunc()
 }
 
-func runner(done chan bool, rerun chan bool) {
-	cmd, cancel := run("hello", "world")
-	go cmd.Run() // TODO: fix me
+func start(done <-chan bool) {
+	r := NewRunnerWithDefault(os.Args[1], os.Args[2:]...)
+	r.Run()
+	fileChanged := make(chan bool)
+	w := NewWatcher(".")
+	go w.Watch(fileChanged)
+
 	for {
 		select {
 		case <-done:
+			w.done <- true
+			r.Exit()
 			return
-		case <-rerun:
-			cancel()
-			cmd, cancel = run("hello", "world")
-			go cmd.Run()
+		case <-fileChanged:
+			r.ReRun()
 		}
 	}
 }
 
 func main() {
-	r := NewRunnerWithDefault("echo", "Hello", "World")
-	r.Run()
-	time.Sleep(1 * time.Second)
-	r.ReRun()
-	time.Sleep(5 * time.Second)
+	// time.Sleep(5 * time.Second)
 	// app := &cli.App{
 	// 	Name:  "runr",
 	// 	Usage: "run any command when file changes",
@@ -140,4 +157,10 @@ func main() {
 	// 	},
 	// }
 	// app.Run(os.Args)
+	done := make(chan bool)
+	go start(done)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c // wait for user to press CTRL+C
+	done <- true
 }
